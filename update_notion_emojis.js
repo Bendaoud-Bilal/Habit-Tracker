dad167eb381a0253ebf1a99a10f29a31b98ba32b
Bendaoud-Bilal <bendaoud.bilal@univ-ouargla.dz>
2026-08-12 14:34:57 +0100

    update habits weights

--- update_notion_emojis.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const notion = require('./notion');
-
-async function run() {
-  const schema = await notion.loadSchema();
-  console.log("Current habit names in Notion:");
-  schema.habitProps.forEach(h => console.log(`- ${h.name}`));
-}
-run();
